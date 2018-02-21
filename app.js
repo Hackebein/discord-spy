de52450e225d8cccc9d2588837e2b0d9f4c8a913
Lars Olzem <hackebein@gmail.com>
2018-02-21 02:22:17 +0000

    add project files

---
+++ app.js
@@ -0,0 +1,157 @@
+const Discord = require("discord.js");
+const bot = new Discord.Client();
+const config = require('./settings.json');
+let channel_log;
+
+function getChannelPath(channel) {
+    let path = "";
+    if ("parent" in channel && channel.parent) {
+        path += channel.parent.name;
+    }
+    path += "#" + channel.name + "@" + channel.guild.name;
+    return path;
+}
+
+function getChannelIcon(channel) {
+    return channel.guild.iconURL;
+}
+
+function getFooter(user) {
+    return {
+        icon_url: user.avatarURL,
+        text: user.username
+    };
+}
+
+function isInit() {
+    return channel_log;
+}
+
+function isUserEquivalent(user1, user2) {
+    return user1.id === user2.id;
+}
+
+function getRichEmbed(obj) {
+    return new Discord.RichEmbed(Object.assign({}, {
+        color: 0x31B0D5,
+        timestamp: new Date()
+    }, obj));
+}
+
+// TODO: testing
+/*
+bot.on("disconnect", () => {
+    channel_log = undefined;
+
+    channel_log.send(getRichEmbed({
+        title: "Exit",
+        footer: getFooter(bot)
+    })).catch(console.error);
+});
+*/
+
+bot.on("message", (message) => {
+    if(!isInit() || isUserEquivalent(bot.user, message.author)) return;
+
+    let richEmbed = getRichEmbed({
+        author: {
+            name: getChannelPath(message.channel),
+            icon_url: getChannelIcon(message.channel)
+        },
+        color: 0x218838,
+        description: message.cleanContent,
+        timestamp: new Date(message.createdAt),
+        title: "New message",
+        footer: getFooter(message.author)
+    });
+
+    message.attachments.forEach(function (attachment) {
+        richEmbed.attachFile(attachment.url);
+    });
+
+    channel_log.send(richEmbed).catch(console.error);
+});
+
+bot.on("messageDelete", (message) => {
+    if(!isInit() || isUserEquivalent(bot.user, message.author)) return;
+
+    let richEmbed = getRichEmbed({
+        author: {
+            name: getChannelPath(message.channel),
+            icon_url: getChannelIcon(message.channel)
+        },
+        color: 0xC9302C,
+        description: message.cleanContent,
+        timestamp: new Date(message.createdAt),
+        title: "Delete message",
+        footer: getFooter(message.author)
+    });
+
+    message.attachments.forEach(function (attachment) {
+        richEmbed.attachFile(attachment.url);
+    });
+
+    channel_log.send(richEmbed).catch(console.error);
+});
+
+bot.on("messageUpdate", (oldMessage, newMessage) => {
+    if(!isInit() || isUserEquivalent(bot.user, oldMessage.author)) return;
+
+    let oldRichEmbed = getRichEmbed({
+        author: {
+            name: getChannelPath(oldMessage.channel),
+            icon_url: getChannelIcon(oldMessage.channel)
+        },
+        color: 0xEC971F,
+        description: oldMessage.cleanContent,
+        timestamp: new Date(oldMessage.createdAt),
+        title: "Update message",
+        footer: getFooter(oldMessage.author)
+    });
+
+    oldMessage.attachments.forEach(function (attachment) {
+        oldRichEmbed.attachFile(attachment.url);
+    });
+
+    channel_log.send(oldRichEmbed).catch(console.error);
+
+    let newRichEmbed = getRichEmbed({
+        author: {
+            name: getChannelPath(newMessage.channel),
+            icon_url: getChannelIcon(newMessage.channel)
+        },
+        color: 0xEC971F,
+        description: newMessage.cleanContent,
+        timestamp: new Date(newMessage.createdAt),
+        title: "Updated message",
+        footer: getFooter(newMessage.author)
+    });
+
+    newMessage.attachments.forEach(function (attachment) {
+        newRichEmbed.attachFile(attachment.url);
+    });
+
+    channel_log.send(newRichEmbed).catch(console.error);
+});
+
+bot.on("ready", () => {
+    bot.user.setStatus("invisible").catch(console.error);
+    channel_log = bot.channels.find("id", config.channel_log);
+
+    channel_log.send(getRichEmbed({
+        title: "Started",
+        footer: getFooter(bot)
+    })).catch(console.error);
+});
+
+bot.on("reconnecting", () => {
+    bot.user.setStatus("invisible").catch(console.error);
+    channel_log = bot.channels.find("id", config.channel_log);
+
+    channel_log.send(getRichEmbed({
+        title: "Reconnected",
+        footer: getFooter(bot)
+    })).catch(console.error);
+});
+
+bot.login(config.token).catch(console.error);
